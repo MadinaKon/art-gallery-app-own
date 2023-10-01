9c253adb508b35c81795be6857ad49d6e2c664b3
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-10-01 19:13:40 +0200

    draft of user story 1

--- art-gallery-app/pages/ArtPiecePreview.js
+++ art-gallery-app/pages/ArtPiecePreview.js
@@ -2,10 +2,10 @@ import React from "react";
 import Image from "next/image";
 
 export default function ArtPiecePreview({ image, title, artist }) {
-  console.log("image, title, artist ", image, title, artist);
   return (
     <>
       <div>ArtPiecePreview</div>
+      {title} {artist}
       <Image src={image} height={144} width={144} alt={title} />
     </>
   );
